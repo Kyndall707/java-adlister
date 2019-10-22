a286665f75ad9d16f426b5a1d8ffc3cb12182da6
Kyndall Sims <sims.kyndall22@gmail.com>
2019-10-22 09:34:12 -0500

    finished

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet("/hello-world")
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
